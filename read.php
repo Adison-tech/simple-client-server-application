55ecffb62dc9f6562ab246a23b4eaafeefd46cb5
Adison Cheruiyot <adisoncheruiyot55@gmail.com>
2025-07-01 21:22:58 +0300

    feat: Function/File to fetch records from the database and display

--- read.php
+++ read.php
@@ -1,27 +1,16 @@
 <?php 
-//database conection  file 
 include('dbconnection.php'); 
-//Code for deletion 
-if(isset($_GET['delid'])) 
-  { 
-  $rid=intval($_GET['delid']); 
-  $sql=mysqli_query($con,"delete from tblusers where ID=$rid"); 
-  echo "<script>alert('Data deleted');</script>";
-  echo "<script>window.location.href = 'index.php'</script>";      
-  }  
 ?> 
 <!DOCTYPE html> 
 <html lang="en"> 
 <head> 
   <meta charset="utf-8"> 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
-  <title>Bootstrap Elegant Table Design</title> 
+  <title>Crud Operation Using PHP and MySQLi</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round"> 
-  <link rel="stylesheet" 
-  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> 
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font
-  awesome.min.css"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font wesome.min.css"> 
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script> 
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> 
@@ -53,8 +42,8 @@ if(isset($_GET['delid']))
   .table-title select { 
     border-color: #ddd; 
     border-width: 0 0 1px 0; 
-    padding: 3px 10px 3px 5px;
-     margin: 0 5px; 
+    padding: 3px 10px 3px 5px; 
+    margin: 0 5px; 
   } 
   .table-title .show-entries { 
     margin-top: 7px; 
@@ -148,7 +137,7 @@ if(isset($_GET['delid']))
     background: #0397d6; 
   } 
   .pagination li.disabled i { 
-   color: #ccc; 
+    color: #ccc; 
   } 
   .pagination li i { 
     font-size: 16px; 
@@ -168,60 +157,44 @@ if(isset($_GET['delid']))
         <div class="table-title"> 
           <div class="row"> 
             <div class="col-sm-5"> 
-              <h2>User <b>Management</b></h2> 
+              <h2>User <b>Details</b></h2> 
             </div> 
-            <div class="col-sm-7" align="right"> 
-              <a href="insert.php" class="btn btn-secondary"><i class="material-icons">&#xE147;</i><span>Add New User</span></a> 
-                                         
-            </div> 
+                      
           </div> 
         </div> 
-        <table class="table table-striped table-hover"> 
-          <thead> 
-            <tr> 
-              <th>#</th> 
-                <th>Name</th>                        
-                <th>Email</th> 
-                <th>Mobile Number</th> 
-                <th>Created Date</th> 
-                <th>Action</th> 
-            </tr> 
-          </thead> 
+        <table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered" id="hidden able-info">   
           <tbody> 
             <?php 
-              $ret=mysqli_query($con,"select * from tblusers"); 
+              $vid=$_GET['viewid']; 
+              $ret=mysqli_query($con,"select * from tblusers where ID =$vid"); 
               $cnt=1; 
-              $row=mysqli_num_rows($ret); 
-              if($row>0){ 
               while ($row=mysqli_fetch_array($ret)) { 
-              
             ?> 
-
             <tr> 
-              <td><?php echo $cnt;?></td> 
-              <td><?php  echo $row['FirstName'];?> <?php echo $row['LastName'];?></td> 
-              <td><?php  echo $row['Email'];?></td>                         
+              <th>First Name</th> 
+              <td><?php  echo $row['FirstName'];?></td> 
+              <th>Last Name</th> 
+              <td><?php  echo $row['LastName'];?></td> 
+            </tr> 
+            <tr> 
+              <th>Email</th> 
+              <td><?php  echo $row['Email'];?></td> 
+              <th>Mobile Number</th> 
               <td><?php  echo $row['MobileNumber'];?></td> 
-              <td> <?php  echo $row['CreationDate'];?></td> 
-              <td><a href="read.php?viewid=<?php echo htmlentities ($row['ID']);?>" class="view" title="View" data toggle="tooltip"><i class="material-icons">&#xE417;</i></a> 
-              <a href="edit.php?editid=<?php echo htmlentities ($row['ID']);?>" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a> 
-              <a href="index.php?delid=<?php echo ($row['ID']);?>" class="delete" title="Delete" data-toggle="tooltip" onclick="return confirm('Do you really want to Delete ?');"><i class="material icons">&#xE872;</i></a> 
-              </td> 
+            </tr> 
+            <tr> 
+              <th>Address</th> 
+              <td><?php  echo $row['Address'];?></td> 
+              <th>Creation Date</th> 
+              <td><?php  echo $row['CreationDate'];?></td> 
             </tr> 
             <?php  
-              $cnt=$cnt+1; 
-              } } else {
-            ?> 
-            <tr> 
-              <th style="text-align:center; color:red;" colspan="6">No Record Found</th> 
-            </tr> 
-            <?php } ?>                  
-                 
+            $cnt=$cnt+1; 
+            }?>      
           </tbody> 
         </table> 
-        
       </div> 
     </div> 
   </div>      
 </body> 
-</html> 
+</html>
